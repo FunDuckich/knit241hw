081c8d5128fb212e8f91daaa23638a72c0315321
FunDuckich <petrovegor495@gmail.com>
2025-03-22 11:10:14 +0400

    сделал 6

--- src/main/java/org/knit/solutions/Task6.java
+++ src/main/java/org/knit/solutions/Task6.java
@@ -1,12 +1,14 @@
 package org.knit.solutions;
 
 
+import org.knit.TaskDescription;
 import org.knit.solutions.lab2sem2.CarInTrainTask;
 import org.knit.solutions.lab2sem2.Railroad;
 import org.knit.solutions.lab2sem2.Train;
 
-public class Task6 {
-    public static void execute() throws InterruptedException {
+@TaskDescription(taskNumber = 6, taskDescription = "Железнодорожный переезд", href = "org/knit/solutions/taskExampleClasses/README.md")
+public class Task6 implements Solution {
+    public void execute() {
         Railroad railroad = new Railroad();
         Train train = new Train(railroad);
         CarInTrainTask car = new CarInTrainTask(railroad);
